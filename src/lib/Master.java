78baa213906b1159a63e25a07632b0779c06068c
Tholut Akhyar <TholutAkhyarZunvou@Gmail.Com>
2018-04-14 08:49:37 +0700

    Initial commit

---
+++ src/lib/Master.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib;
+
+import gui.DashboardFrame;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/*
+------------------- Master --------------------
+
+Deskripsi:
+Semuanya di panggil dari sini, meskipun bisa di panggil dalam satu script.
+
+------------------ Informasi ------------------
+
+Rank:
+1 = Pemilik
+2 = Karyawan
+3 = Pelanggan (Tidak Bisa Masuk)
+
+*/
+
+/**
+ *
+ * @author TholutAkhyar
+ */
+public class Master {
+    ResultSet MasterResultSet;
+    lib.MySQL MySQL = new lib.MySQL();
+    
+    public String RankName(int Rank) {
+        String resultCv = "";
+                
+        if (Rank == 1) {
+            resultCv = "Pemilik";
+        } else if (Rank == 2) {
+            resultCv = "Karyawan";
+        } else if (Rank == 3) {
+            resultCv = "Pelanggan";
+        } else {
+            resultCv = "Tidak Ada";
+        }
+        
+        return resultCv;
+    }
+    
+    public void showTholutDialogOk(String title, String msg, String icon) {
+        gui.DialogOkFrame DialogOk = new gui.DialogOkFrame();
+        String DataIcon = "LogoInformation.png";
+        String DataTitle = "Information";
+        String DataMessage = "No message";
+        
+        if (null == icon) {
+            DataIcon = "LogoInformation.png";
+        } else switch (icon) {
+            case "Warning":
+                DataIcon = "LogoWarning.png";
+                break;
+            case "Error":
+                DataIcon = "LogoError.png";
+                break;
+            case "Information":
+                DataIcon = "LogoInformation.png";
+                break;
+            case "Success":
+                DataIcon = "LogoSuccess.png";
+                break;
+            case "Failed":
+                DataIcon = "LogoFailed.png";
+                break;
+            case "Ask":
+                DataIcon = "LogoAsk.png";
+                break;
+            default:
+                DataIcon = "LogoInformation.png";
+                break;
+        }
+        
+        if ("".equals(title)) {
+            DataTitle = "Information";
+        } else {
+            DataTitle = title;
+        }
+        
+        if ("".equals(msg)) {
+            DataMessage = "No message";
+        } else {
+            DataMessage = msg;
+        }
+        
+        DialogOk.MasterDialogOk(DataTitle, DataMessage, DataIcon);
+    }
+    
+    public void showTholutDashboard(int User_ID) {
+        gui.DashboardFrame Dashboard = new gui.DashboardFrame();
+        String data_dash[] = {"Title", "Nama", "Jabatan"};
+        try {
+            
+            String sql = "select * from user where user_id = '"+User_ID+ "'";
+            
+            MasterResultSet = MySQL.MySQLQuery(sql);
+            
+            if (MasterResultSet.next()) {
+                data_dash[0] = MasterResultSet.getString("user_id");
+                data_dash[1] = MasterResultSet.getString("username");
+                data_dash[2] = MasterResultSet.getString("user_jabatan");
+                
+                System.out.println("UserID: "+MasterResultSet.getString("user_id"));
+            } else {
+                showTholutDialogOk("Informasi", "Maaf user ini tidak ada", "Error");
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
+        Dashboard.MasterDashboard(Integer.parseInt(data_dash[0]), data_dash[1], Integer.parseInt(data_dash[2]));
+    }
+
+}
---
+++ src/lib/MySQL.java
@@ -0,0 +1,190 @@
+/*
+ * Name         : dbconnect.java
+ * Package      : libs
+ * Written By   : Tholut Akhyar
+ * Date Created : 03 - 06 - 2018
+ * Date Updated : 03 - 06 - 2018
+ */
+
+package lib;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author TholutAkhyar
+ */
+
+public class MySQL {
+    
+    public static Connection MySQLConn;
+    public static Statement MySQLStatement;
+    public static ResultSet MySQLResultSet;
+    
+    static String MySQLDriver = "com.mysql.jdbc.Driver";
+    static String MySQLUrl = "jdbc:mysql://localhost/";
+    static String MySQLDb = "apkg_db";
+    static String MySQLUser = "root";
+    static String MySQLPass = "1234";
+    
+    // Checking if you are connected with database
+    public static Connection MySQLConnection() {
+        if (MySQLConn == null) {
+            try {
+                Class.forName(MySQLDriver);
+                MySQLConn = (Connection) DriverManager.getConnection(MySQLUrl+MySQLDb, MySQLUser, MySQLPass);
+                MySQLStatement = (Statement) MySQLConn.createStatement(); 
+                System.out.println("[Lib][MySQL] Connection Success");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.err.println("[Lib][MySQL] Connection Failed [ "+e.getMessage()+" ]");
+            }
+        }
+        return MySQLConn;
+    }
+    
+    // Method called for do Query Select
+    public ResultSet MySQLQuery(String sql) throws SQLException {
+        Connection Conn = (Connection) MySQL.MySQLConnection();
+        MySQLResultSet = MySQLStatement.executeQuery(sql);
+        
+        return MySQLResultSet;
+    }
+    
+    // Method called for do Query Insert, Update, Delete
+    public int MySQLUpdate(String sql) throws SQLException {
+        int SuccessUpdate;
+        Connection Conn = (Connection) MySQL.MySQLConnection();
+        Statement Stm = (Statement) Conn.createStatement();
+        SuccessUpdate = MySQLStatement.executeUpdate(sql);
+        
+        return SuccessUpdate;
+    }
+    
+    // Method called for get Database Kaset
+    public String[][] MySQLGetUser(String FindWhat, String TypeWhat, String UserJabatan) throws SQLException {
+        Connection Conn = (Connection) MySQL.MySQLConnection();
+        
+        // Checking Type of Data
+        if (null != TypeWhat) switch (TypeWhat) {
+            case "User Id":
+                TypeWhat = "user_id";
+                break;
+            case "Nama":
+                TypeWhat = "user_namalengkap";
+                break;
+            case "NIK":
+                TypeWhat = "user_nik_ktp";
+                break;
+            default:
+                TypeWhat = "user_id";
+                break;
+        }
+            
+        String[] d_as = {FindWhat, TypeWhat};
+        String query = "select * from user";
+        
+        if (!"".equals(d_as[0]) && !"".equals(d_as[1])) {
+            query = "select * from user where "+d_as[1]+" like '%"+d_as[0]+"%'";
+        }
+        
+        if (!"Pemilik".equals(UserJabatan)) {
+            query = "select * from user where user_jabatan not like '1' and user_jabatan not like '2'";
+            
+            if (!"".equals(d_as[0]) && !"".equals(d_as[1])) {
+                query = "select * from user where "+d_as[1]+" like '%"+d_as[0]+"%' and user_jabatan not like '1' and user_jabatan not like '2'";
+            }
+        }
+        
+        
+        // Counting Data
+        System.out.println("[Lib][MySQL] Running: ["+query+"]");
+        MySQLResultSet = MySQLStatement.executeQuery(query);
+        MySQLResultSet.last(); 
+        int total = MySQLResultSet.getRow();
+        String returnData[][] = new String[total][11];
+        System.out.println("[Lib][MySQL] Total Data ["+total+"]");
+        
+        
+        // Inserting Data to Array
+        System.out.println("[Lib][MySQL] Running: ["+query+"]");
+        MySQLResultSet = MySQLStatement.executeQuery(query);
+        int x = 0;
+        while (MySQLResultSet.next()) {
+             returnData[x][0] = MySQLResultSet.getString("user_id");
+             returnData[x][1] = MySQLResultSet.getString("username");
+             returnData[x][2] = MySQLResultSet.getString("password");
+             returnData[x][3] = MySQLResultSet.getString("user_namalengkap");
+             returnData[x][4] = MySQLResultSet.getString("user_alamat");
+             returnData[x][5] = MySQLResultSet.getString("user_nik_ktp");
+             returnData[x][6] = MySQLResultSet.getString("user_telp");
+             returnData[x][7] = MySQLResultSet.getString("user_email");
+             returnData[x][8] = MySQLResultSet.getString("user_jabatan");
+             returnData[x][9] = MySQLResultSet.getString("user_tgl_lahir");
+             returnData[x][10] = MySQLResultSet.getString("user_tgl_masuk");
+             x++;
+        }
+        System.out.println("[Lib][MySQL] "+total+" Data User Success Loaded");
+        
+        return returnData;
+    }
+    
+    // Method called for get Database Kaset
+    public String[][] MySQLGetKaset(String FindWhat, String TypeWhat) throws SQLException {
+        Connection Conn = (Connection) MySQL.MySQLConnection();
+        
+        // Checking Type of Data
+        if (null != TypeWhat) switch (TypeWhat) {
+            case "Kode":
+                TypeWhat = "kaset_id";
+                break;
+            case "Judul":
+                TypeWhat = "kaset_nama";
+                break;
+            case "Kategori":
+                TypeWhat = "kaset_kategori";
+                break;
+            default:
+                TypeWhat = "kaset_id";
+                break;
+        }
+            
+        String[] d_as = {FindWhat, TypeWhat};
+        String query = "select * from kaset";
+            
+        
+        if (!"".equals(d_as[0]) && !"".equals(d_as[1])) {
+            query = "select * from kaset where "+d_as[1]+" like '%"+d_as[0]+"%'";
+        }
+        
+        
+        // Counting Data
+        System.out.println("[Lib][MySQL] Running: ["+query+"]");
+        MySQLResultSet = MySQLStatement.executeQuery(query);
+        MySQLResultSet.last(); 
+        int total = MySQLResultSet.getRow();
+        String returnData[][] = new String[total][6];
+        System.out.println("[Lib][MySQL] Total Data ["+total+"]");
+        
+        
+        // Inserting Data to Array
+        System.out.println("[Lib][MySQL] Running: ["+query+"]");
+        MySQLResultSet = MySQLStatement.executeQuery(query);
+        int x = 0;
+        while (MySQLResultSet.next()) {
+             returnData[x][0] = MySQLResultSet.getString("kaset_id");
+             returnData[x][1] = MySQLResultSet.getString("kaset_nama");
+             returnData[x][2] = MySQLResultSet.getString("kaset_jumlah_keping");
+             returnData[x][3] = MySQLResultSet.getString("kaset_kategori");
+             returnData[x][4] = MySQLResultSet.getString("kaset_stok");
+             returnData[x][5] = MySQLResultSet.getString("kaset_harga");
+             x++;
+        }
+        System.out.println("[Lib][MySQL] "+total+" Data Kaset Success Loaded");
+        
+        return returnData;
+    }
+}
